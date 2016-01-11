ef48b92f6cefc21771d8f42abf1064f186ddfc04
dannypier <dannypier@gmail.com>
2016-01-11 14:41:38 -0700

    Moving everything to parent directory

--- app.js
+++ app.js
@@ -1,70 +1,60 @@
-angular.module('flapperNews', ['ui.router'])
-    .config([
-        '$stateProvider',
-        '$urlRouterProvider',
-        function($stateProvider, $urlRouterProvider){
-            $stateProvider.state('home', {
-                url: '/home',
-                templateUrl: '/home.html',
-                controller: 'MainCtrl'
-            })
-                .state('posts', {
-                url: '/posts/{id}',
-                templateUrl: '/posts.html',
-                controller: 'PostsCtrl'
-            })
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-            $urlRouterProvider.otherwise('home')
-        }
-    ])
-    .factory('posts', [function(){
-    var o = {
-        posts: [ ]
-    }
-    return o;
-}])
-.controller('MainCtrl', [
-    '$scope',
-    'posts',
-    function($scope, posts){
-        $scope.test = 'Hello world!';
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
-        $scope.posts = posts.posts;
+var app = express();
 
-        $scope.addPost = function(){
-            if ($scope.title == '' || $scope.link == '') { return ;}
-            $scope.posts.push( {
-                title: $scope.title,
-                link: $scope.link,
-                upvotes:1,
-                comments: []
-            })
-            $scope.title=''
-            $scope.link=''
-        }
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-        $scope.incrementUpvotes = function(post){
-            post.upvotes += 1;
-        }
-    }])
-    .controller('PostsCtrl', [
-        '$scope',
-        '$stateParams',
-        'posts',
-        function($scope, $stateParams, posts){
-            $scope.post = posts.posts[$stateParams.id];
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-            $scope.addComment = function(){
-                if ($scope.body == '') { return ;}
-                $scope.post.comments.push(
-                        {
-                            author: '',
-                            body : $scope.body,
-                            upvotes: 1
-                        }
-                );
-                $scope.body=''
+app.use('/', routes);
+app.use('/users', users);
 
-            }
-        }
-    ]);
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
