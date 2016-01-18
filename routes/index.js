f81b3273d87e975b6ccd4c8c2470103f0e89ddce
dannypier <dannypier@gmail.com>
2016-01-18 13:11:03 -0700

    Creating register and login endpoints

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
+var passport = require('passport');
 var router = express.Router();
+var User = mongoose.model('User');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -105,5 +107,45 @@ router.put('/posts/:post/comments/:comment/upvote', function(req,res,next){
         res.json(comment)
     });
 });
+
+/* USER */
+
+router.post('/register', function(req, res, next){
+    if(!req.body.email || !req.body.password){
+        return res.status(400).json({message: 'Email and password parameters required'});
+    }
+
+    var user = new User();
+
+    user.email = req.body.email;
+
+    user.setPassword(req.body.password);
+
+    user.save(function (err){
+
+        if (err) { return next(err); }
+
+        return res.json({token : user.generateJWT()})
+    });
+});
+
+router.post('/login', function(req, res, err){
+    if (!req.body.email || !req.body.password){
+        return res.status(400).json({message: 'Email and password parameters required'});
+    }
+
+    passport.authenticate('local', function(err, user, info){
+        if (err){ return next(err); }
+
+        if (user){
+            return res.json({token: user.generateJWT()});
+        } else {
+            return res.status(401).json(info);
+        }
+    })(req, res, next);
+})
+
+
+
 
 module.exports = router;
